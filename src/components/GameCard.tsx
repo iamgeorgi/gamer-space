8612f3cd6d4e536150ad92b286555951cc33f662
iamgeorgi <georgipeetrov@gmail.com>
2026-06-29 17:22:48 +0300

    move the game card ui in separate component

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,31 @@
+import { Badge, Card, HStack, Image, Text } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
+import type { GameCard } from "@/services/games-service";
+
+interface Props {
+  game: GameCard;
+}
+
+function GameCard({ game }: Props) {
+  return (
+    <Card.Root key={game.id} maxW="sm" overflow="hidden">
+      <Image src={game.background_image} alt={game.name} />
+      <Card.Body gap="2">
+        <HStack justifyContent="space-between">
+          <PlatformIconList
+            platforms={game.parent_platforms?.map((p) => p.platform)}
+          />
+          <Badge px="13px" py="5px" colorPalette="green">
+            {game.metacritic}
+          </Badge>
+        </HStack>
+        <Text textStyle="2xl" fontWeight="medium" letterSpacing="tight" mt="2">
+          {game.name}
+        </Text>
+      </Card.Body>
+      <Card.Footer gap="2"></Card.Footer>
+    </Card.Root>
+  );
+}
+
+export default GameCard;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,18 +1,14 @@
 import useGames from "@/hooks/useGames";
-import type { GameCard } from "@/services/games-service";
+import type { GameCard as GameCardType } from "@/services/games-service";
 import {
-  Card,
   SimpleGrid,
-  Image,
   Text,
   Spinner,
-  Badge,
-  HStack,
 } from "@chakra-ui/react";
-import PlatformIconList from "./PlatformIconList";
+import GameCard from "./GameCard";
 
 interface Props {
-    games?: GameCard[]
+    games?: GameCardType[]
 }
 
 function GameGrid({ games }: Props) {
@@ -23,26 +19,7 @@ function GameGrid({ games }: Props) {
       {error && <Text bg="bg.error">{error}</Text>}
       {isLoading && <Spinner />}
       {games?.map((game) => (
-        <Card.Root key={game.id} maxW="sm" overflow="hidden">
-          <Image src={game.background_image} alt={game.name} />
-          <Card.Body gap="2">
-            <HStack>
-              <Badge px="13px" py="5px" colorPalette="green">{game.metacritic}</Badge>
-            </HStack>
-            <Text
-              textStyle="2xl"
-              fontWeight="medium"
-              letterSpacing="tight"
-              mt="2"
-            >
-              {game.name}
-            </Text>
-            <HStack>
-                <PlatformIconList platforms={game.parent_platforms?.map(p => p.platform)} />
-            </HStack>
-          </Card.Body>
-          <Card.Footer gap="2"></Card.Footer>
-        </Card.Root>
+        <GameCard game={game} />
       ))}
     </SimpleGrid>
   );
